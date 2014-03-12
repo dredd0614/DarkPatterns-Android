f7aa968c49923ba691630bee8dc9cdd6c77381ae
dredd0614 <redding.dave@gmail.com>
2014-03-12 15:19:13 -0400

    init commit for pull-to-refresh

---
+++ src/co/touchlab/dark/MyActivity.java
@@ -0,0 +1,26 @@
+package co.touchlab.dark;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+
+    }
+
+
+    public void launchListView(View v){
+        startActivity(new Intent(this, ListViewActivity.class));
+    }
+
+}
